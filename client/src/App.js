d93a2f7b66793fc607b4f0f55fdbea290e9426c9
Won Lee <whd793@gmail.com>
2024-08-17 12:42:03 +0900

    added product card component and shop page and context

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,17 @@
-import { useContext } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-import { UserContext } from './contexts/user.context';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
-  const { currentUser } = useContext(UserContext);
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route
-          path='auth'
-          element={
-            currentUser ? <Navigate to='/' replace /> : <Authentication />
-          }
-        />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
